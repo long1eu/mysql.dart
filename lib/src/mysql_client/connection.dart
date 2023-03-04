ca5465bc4c42bb14eb9ac8981bc64f5c88af0b85
Razvan Cristian Lung <lung.razvan@gmail.com>
2023-03-04 17:29:37 +0200

    move substituteParams to be accessed no mater is a pool or just one connection

--- lib/src/mysql_client/connection.dart
+++ lib/src/mysql_client/connection.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:mysql_client/mysql_protocol.dart';
 import 'package:mysql_client/exception.dart';
+import 'package:mysql_client/src/mysql_client/util.dart';
 
 enum _MySQLConnectionState {
   fresh,
@@ -662,71 +663,6 @@ class MySQLConnection {
     }
   }
 
-  String substituteParams(String query, Map<String, dynamic> params) {
-    // convert params to string
-    Map<String, String> convertedParams = {};
-
-    for (final param in params.entries) {
-      String value;
-
-      if (param.value == null) {
-        value = "NULL";
-      } else if (param.value is String) {
-        value = "'" + _escapeString(param.value) + "'";
-      } else if (param.value is num) {
-        value = param.value.toString();
-      } else if (param.value is bool) {
-        value = param.value ? "TRUE" : "FALSE";
-      } else {
-        value = "'" + _escapeString(param.value.toString()) + "'";
-      }
-
-      convertedParams[param.key] = value;
-    }
-
-    // find all :placeholders, which can be substituted
-    final pattern = RegExp(r":(\w+)");
-
-    final matches = pattern.allMatches(query).where((match) {
-      final subString = query.substring(0, match.start);
-
-      int count = "'".allMatches(subString).length;
-      if (count > 0 && count.isOdd) {
-        return false;
-      }
-
-      count = '"'.allMatches(subString).length;
-      if (count > 0 && count.isOdd) {
-        return false;
-      }
-
-      return true;
-    }).toList();
-
-    int lengthShift = 0;
-
-    for (final match in matches) {
-      final paramName = match.group(1);
-
-      // check param exists
-      if (false == convertedParams.containsKey(paramName)) {
-        throw MySQLClientException(
-            "There is no parameter with name: $paramName");
-      }
-
-      final newQuery = query.replaceFirst(
-        match.group(0)!,
-        convertedParams[paramName]!,
-        match.start + lengthShift,
-      );
-
-      lengthShift += newQuery.length - query.length;
-      query = newQuery;
-    }
-
-    return query;
-  }
-
   /// Prepares given [query]
   ///
   /// Returns [PreparedStmt] which can be used to execute prepared statement multiple times with different parameters
@@ -1062,12 +998,6 @@ class MySQLConnection {
     );
 
     _socket.add(packet.encode());
-  }
-
-  String _escapeString(String value) {
-    value = value.replaceAll(r"\", r'\\');
-    value = value.replaceAll(r"'", r"''");
-    return value;
   }
 
   /// Close this connection gracefully
